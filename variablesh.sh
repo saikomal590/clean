c10bcf3d803a57604d9c1bd6684ee431c3e6d7dd
deppu <deepu@gmail.com>
2024-09-10 10:39:05 +0530

    some message

--- variablesh.sh
+++ variablesh.sh
@@ -1,1 +1,7 @@
-git to be pushed 
+#!/bin/bash
+
+echo "Ramesh:: Hello Suresh, How are you?"
+echo "Suresh:: Hi Ramesh, I am fine. How are you?"
+echo "Ramesh: I am fine too. how is your work?"
+echo "Suresh:: not bad. I am thinking to upgrade to DevOps"
+
